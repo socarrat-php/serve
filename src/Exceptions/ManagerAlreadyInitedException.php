14e8f95ca585c6f67f0c83233dbba918e45af5d0
Romein van Buren <romein@vburen.nl>
2023-05-07 16:36:21 +0200

    Removed manager exceptions

--- src/Exceptions/ManagerAlreadyInitedException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Socarrat\Serve\Exceptions;
-
-class ManagerAlreadyInitedException extends \Exception {
-    public function __construct(string $managerName, $code = 0, \Throwable $previous = null) {
-        parent::__construct($managerName." can only be initialised once.", $code, $previous);
-    }
-}
--- src/Exceptions/ManagerNotInitedException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Socarrat\Serve\Exceptions;
-
-class ManagerNotInitedException extends \Exception {
-    public function __construct(string $managerName, $code = 0, \Throwable $previous = null) {
-        parent::__construct($managerName." is not yet initialised.", $code, $previous);
-    }
-}
